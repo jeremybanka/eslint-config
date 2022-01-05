2222f2d997ab609a503157698f851db3fcfb9d9c
Jeremy Banka <hello@jeremybanka.com>
2022-01-04 18:10:29 -0800

    🔧 allow explicit any

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     "@typescript-eslint/type-annotation-spacing": `error`,
     "@typescript-eslint/unified-signatures": `error`,
     "@typescript-eslint/no-unused-vars": `off`,
+    "@typescript-eslint/no-explicit-any": `off`,
     "@typescript-eslint/explicit-module-boundary-types": `error`,
     "@typescript-eslint/consistent-type-imports": [
       `error`,
